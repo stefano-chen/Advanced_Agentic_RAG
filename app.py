91b7779ab18fabd9a9181f3451b9f4cf6837649b
stefano-chen <stefanochen01@gmail.com>
2025-09-18 12:45:41 +0200

    refactoring

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from langgraph.graph import StateGraph, START, END, MessagesState
-from langgraph.prebuilt import ToolNode
 from pathlib import Path
 import json
 from utils.processing import get_topics, save_to_png, stream_response
 from utils.llm import LLMModel
-from nodes.query_validation import QueryValidation, is_related
-from dotenv import load_dotenv
-from nodes.tool_calling import ToolCalling, tool_condition
 from tools.retrieval import get_tools
+from utils.agent import build_agent
+from dotenv import load_dotenv
 
-load_dotenv("./.env")
 
 if __name__ == "__main__":
+
+    load_dotenv("./.env")
 
     if not Path("./config/app_config.json").exists():
         raise FileNotFoundError("app_config.json not Found")
@@ -31,34 +29,7 @@ if __name__ == "__main__":
 
     tools = get_tools(app_config["vector_db"], app_config["db_dir_path"], app_config["k"], app_config["embedding_provider"], app_config["embedding_model"], app_config["embedding_host"])
 
-    graph = StateGraph(MessagesState)
-
-    # Insert Graph Node and Edges
-    graph.add_node("validate_input", QueryValidation(llm, prompts["input_check"], topics).validate)
-    graph.add_node("retrieve_or_respond", ToolCalling(llm, prompts["retrieve_respond"], tools).choose)
-    graph.add_node("tool_execution", ToolNode(tools))
-
-    graph.add_edge(START, "validate_input")
-    graph.add_conditional_edges(
-        "validate_input",
-        is_related,
-        {
-            "yes": "retrieve_or_respond",
-            "no": END
-        }
-    )
-
-    graph.add_conditional_edges(
-        "retrieve_or_respond",
-        tool_condition,
-        {
-            "retrieve": "tool_execution",
-            "respond": END
-        }
-    )
-
-    graph.add_edge("tool_execution", "retrieve_or_respond")
-    agent = graph.compile()
+    agent = build_agent(llm, prompts, tools, topics)
 
     save_to_png(agent)
     user_query = input("Enter: ")
---
+++ utils/agent.py
@@ -0,0 +1,32 @@
+from langgraph.graph import StateGraph, START, END, MessagesState
+from langgraph.prebuilt import ToolNode
+from nodes.query_validation import QueryValidation, is_related
+from nodes.tool_calling import ToolCalling, tool_condition
+
+def build_agent(llm, prompts, tools, topics):
+    graph = StateGraph(MessagesState)
+    # Insert Graph Node
+    graph.add_node("validate_input", QueryValidation(llm, prompts["input_check"], topics).validate)
+    graph.add_node("retrieve_or_respond", ToolCalling(llm, prompts["retrieve_respond"], tools).choose)
+    graph.add_node("tool_execution", ToolNode(tools))
+    # Create Edeges
+    graph.add_edge(START, "validate_input")
+    graph.add_conditional_edges(
+        "validate_input",
+        is_related,
+        {
+            "yes": "retrieve_or_respond",
+            "no": END
+        }
+    )
+    graph.add_conditional_edges(
+        "retrieve_or_respond",
+        tool_condition,
+        {
+            "retrieve": "tool_execution",
+            "respond": END
+        }
+    )
+    graph.add_edge("tool_execution", "retrieve_or_respond")
+    # return the compiled graph
+    return graph.compile()
