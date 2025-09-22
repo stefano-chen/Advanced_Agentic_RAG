f96cacc3b4c92666c5566dba707c3c2e869ee3a8
stefano-chen <stefanochen01@gmail.com>
2025-09-22 17:44:30 +0200

    improve prompting

--- nodes/tool_calling.py
+++ nodes/tool_calling.py
@@ -9,18 +9,11 @@ class Choice:
     def __init__(self, llm: BaseChatModel, prompt: str):
         self._llm = llm
         self._prompt = prompt
-
-    def _get_past_tool_calls(self, messages: List[AnyMessage]) -> str:
-        past_tool_calls = ""
-        for msg in messages:
-            if msg.type == "ai" and hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
-                past_tool_calls += str(msg.tool_calls) + "\n"
-        return past_tool_calls
     
     def choose(self, state: AgentState) -> AgentState:
         question = state["original_question"]
         context = state["context"]
-        past_tool_calls = self._get_past_tool_calls(state["messages"])
+        past_tool_calls = _get_past_tool_calls(state["messages"])
         prompt = PromptTemplate.from_template(self._prompt).invoke({"question": question, "context": context, "past_tool_calls": past_tool_calls})
         response = self._llm.invoke(prompt)
         state["messages"].append(response)
@@ -34,14 +27,23 @@ class ToolCalling:
 
     def call(self, state: AgentState) -> AgentState:
         last_msg = state["messages"][-1]
+        past_tool_calls = _get_past_tool_calls(state["messages"])
 
         if "retrieve" in last_msg.content.lower():
-            prompt = PromptTemplate.from_template(self._prompt).invoke({"query": state["question"]})
+            prompt = PromptTemplate.from_template(self._prompt).invoke({"tools": past_tool_calls, "query": state["question"]})
             response = self._llm.invoke(prompt)
             state["messages"].append(response)
         else: 
             state["messages"].append(AIMessage("No tools to call, routing to the generate answer node"))
         return state
+
+
+def _get_past_tool_calls( messages: List[AnyMessage]) -> str:
+    past_tool_calls = ""
+    for msg in messages:
+        if msg.type == "ai" and hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
+            past_tool_calls += str(msg.tool_calls) + "\n"
+    return past_tool_calls
 
 
 def tool_condition(state: AgentState) -> Literal["retrieve", "respond"]:
